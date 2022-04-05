55a9ddb44c7eab84a8d4670f7bb29f8848f1cf31
ethen <ethenyang@126.com>
2022-04-05 15:36:04 +0800

    security config

--- in-depth-security/src/main/java/com/ethen/config/WebSecurityConfig.java
+++ in-depth-security/src/main/java/com/ethen/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @since 2022/04/05
  */
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * bcrypt加密
      *
@@ -40,4 +42,36 @@ public class WebSecurityConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    /**
+     * 配置http安全选项
+     *
+     * <ol>
+     *     <li>静态资源拦截策略</li>
+     *     <li>接口拦截策略</li>
+     *     <li>CSRF安全策略</li>
+     *     <li>登入/登出选项</li>
+     * </ol>
+     *
+     * @param http HttpSecurity
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 表单提交
+        http.formLogin()
+                // 自定义登录页
+//                .loginPage("login.html")
+                // 登录接口路径，须与表单提交一致
+//                .loginProcessingUrl("/user/login")
+                // 认证成功跳转路径
+                .defaultSuccessUrl("/index.html")
+                .and().authorizeRequests()
+                // 设置不需要认证的路径
+                .antMatchers("/user/login", "/login.html").permitAll()
+                // 设置需要认证的路径
+                .anyRequest().authenticated();
+
+        // 关闭CSRF校验
+        http.csrf().disable();
+    }
 }
