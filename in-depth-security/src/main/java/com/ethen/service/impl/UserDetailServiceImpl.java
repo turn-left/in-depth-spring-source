75977da8cef3917156a7a5eb487032856b6c4733
ethen <ethenyang@126.com>
2022-04-05 11:40:52 +0800

    add comment

--- in-depth-security/src/main/java/com/ethen/service/impl/UserDetailServiceImpl.java
+++ in-depth-security/src/main/java/com/ethen/service/impl/UserDetailServiceImpl.java
@@ -12,6 +12,10 @@ import org.springframework.stereotype.Service;
  * 自定义 spring security UserDetailsService 实现
  * <p>
  * fixme 待分析spring security调用链路源码
+ * <p>
+ * note: spring boot 自动配置(spring-boot-autoconfigure)默认是
+ * {@link org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration},
+ * 如果不存在自定义实现，默认就是配置内存用户密码权限。
  *
  * @author ethen
  * @since 2022/04/05
@@ -35,9 +39,11 @@ public class UserDetailServiceImpl implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        // V1.0维护一个内存的用户密码权限，实际通常从数据库获取用户权限数据
         String rawPwd = "root123";
+
         String encodedPwd = passwordEncoder.encode(rawPwd);
+
         System.err.println("encodedPwd:" + encodedPwd);
 
         UserDetails userDetails = User.withUsername("ethen").password(encodedPwd).authorities("admin").build();
