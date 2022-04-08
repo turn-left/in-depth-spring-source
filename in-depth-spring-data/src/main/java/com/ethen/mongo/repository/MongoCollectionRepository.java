67bb173adf441aba7480e051ee03559feae7488d
ethen <ethenyang@126.com>
2022-04-08 23:52:26 +0800

    spring scenarios in action

--- in-depth-spring-data/src/main/java/com/ethen/mongo/repository/MongoCollectionRepository.java
+++ in-depth-spring-data/src/main/java/com/ethen/mongo/repository/MongoCollectionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.validation.Validator;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.Set;
 
 /**
  * mongo文档集合的基本操作
@@ -61,6 +62,22 @@ public class MongoCollectionRepository {
         return mongoTemplate.collectionExists(collName) ? SUCCESS : FAIL;
     }
 
+    /**
+     * 创建【验证文档数据】的集合
+     * <p>
+     * 创建集合并在文档"插入"与"更新"时进行数据效验，如果符合创建集合设置的条件就进允许更新与插入，否则则按照设置的设置的策略进行处理。
+     * <p>
+     * 效验级别：
+     * - off：关闭数据校验。
+     * - strict：(默认值) 对所有的文档"插入"与"更新"操作有效。
+     * - moderate：仅对"插入"和满足校验规则的"文档"做"更新"操作有效。对已存在的不符合校验规则的"文档"无效。
+     * 执行策略：
+     * - error：(默认值) 文档必须满足校验规则，才能被写入。
+     * - warn：对于"文档"不符合校验规则的 MongoDB 允许写入，但会记录一条告警到 mongod.log 中去。日志内容记录报错信息以及该"文档"的完整记录。
+     *
+     * @param collName 集合名称
+     * @return result
+     */
     public Object createValidateCollection(String collName) {
         // 设置验证条件，只允许岁数大于20的用户的信息写入mongo
         CriteriaDefinition criteria = Criteria.where("age").gt(20);
@@ -75,4 +92,31 @@ public class MongoCollectionRepository {
         return mongoTemplate.collectionExists(collName) ? SUCCESS : FAIL;
     }
 
+    /**
+     * 查询集合名称列表
+     *
+     * @return set
+     */
+    public Set<String> listCollections() {
+        return mongoTemplate.getCollectionNames();
+    }
+
+    /**
+     * 根据名称判断是否存在集合
+     *
+     * @param collName
+     * @return
+     */
+    public boolean isExist(String collName) {
+        return mongoTemplate.collectionExists(collName);
+    }
+
+    /**
+     * 删除集合
+     *
+     * @param collName
+     */
+    public void dropCollection(String collName) {
+        mongoTemplate.getCollection(collName).drop();
+    }
 }
