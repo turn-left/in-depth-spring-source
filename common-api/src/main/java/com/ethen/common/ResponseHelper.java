21ba79a122336e4c588132d06555a6bab348554f
ethen <ethenyang@126.com>
2022-04-23 19:02:50 +0800

    usecase csv

--- common-api/src/main/java/com/ethen/common/ResponseHelper.java
+++ common-api/src/main/java/com/ethen/common/ResponseHelper.java
@@ -2,20 +2,28 @@ package com.ethen.common;
 
 import org.springframework.core.env.Environment;
 
+import java.util.Objects;
 
+/**
+ * ResponseHelper
+ */
 public class ResponseHelper {
+
     public static ResponseItem success(Object data, Environment env) {
-
-        String ip = env.getProperty("spring.cloud.client.ip-address");
-        Integer port = env.getProperty("server.port", Integer.class);
-        String service = env.getProperty("spring.application.name");
-        String desc = env.getProperty("eureka.instance.instance-id");
-
         ResponseItem responseItem = new ResponseItem();
         responseItem.setResult(data);
-        responseItem.buildInstanceInfo(ip, port, service, desc);
-
+        if (Objects.nonNull(env)) {
+            String ip = env.getProperty("spring.cloud.client.ip-address");
+            Integer port = env.getProperty("server.port", Integer.class);
+            String service = env.getProperty("spring.application.name");
+            String desc = env.getProperty("eureka.instance.instance-id");
+            responseItem.buildInstanceInfo(ip, port, service, desc);
+        }
         return responseItem;
+    }
+
+    public static ResponseItem success(Object data) {
+        return success(data, null);
     }
 }
 
