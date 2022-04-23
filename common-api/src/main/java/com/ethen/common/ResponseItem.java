2e04a9931f125eb8dc9e2f3f4a172d9abfc80259
ethen <ethenyang@126.com>
2022-04-23 19:04:37 +0800

    response update

--- common-api/src/main/java/com/ethen/common/ResponseItem.java
+++ common-api/src/main/java/com/ethen/common/ResponseItem.java
@@ -17,12 +17,12 @@ public class ResponseItem {
     @ToString
     public static class Instance {
         private String ip;
-        private int port;
+        private Integer port;
         private String service;
         private String desc;
     }
 
-    public void buildInstanceInfo(String ip, int port, String service, String desc) {
+    public void buildInstanceInfo(String ip, Integer port, String service, String desc) {
         instance.setIp(ip);
         instance.setPort(port);
         instance.setService(service);
